7e6fbf33090a53a0c898557ca4a5cc0614c114aa
trammelsol <m15587450902@163.com>
2020-07-16 19:16:48 +0800

    right

--- sizeof_test.cpp
+++ sizeof_test.cpp
@@ -9,7 +9,8 @@ int main ()
   printf("Size of int min %d\n", INT_MIN);
   printf("Size of int max %d\n", INT_MAX);
   printf("Size of long min %ld\n", LONG_MIN);     
-  printf("Size of long max %ld\n", LONG_MAX);      
+  printf("Size of long max %ld\n", LONG_MAX);
+  printf("%d",LONG_BIT);      
   printf("Size of short min %d\n", SHRT_MIN);
   printf("Size of short max %d\n", SHRT_MAX);
   printf("Size of unsigned char %u\n", UCHAR_MAX);  
